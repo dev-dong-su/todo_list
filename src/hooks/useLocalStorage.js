3253869e8708de9428aaa1cc51c3e605fbdb664d
dev-dong-su <fkdlrj1230@gmail.com>
2022-12-08 21:18:48 +0900

    :sparkles: ADD useRequest.js ToDoPage.js, useLocalStorage.js, Validations.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,15 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from 'react';
 
-const useLocalStorage = (key, initialState) => {
-  const [state, setState] = useState(() =>
-    JSON.parse(window.localStorage.getItem(key)) || initialState
-  );
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
 
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(pre => valueToStore);
 
-  return [state, setState];
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return { storedValue, setValue };
 }
 
 export default useLocalStorage;
