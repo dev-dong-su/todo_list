630fae5d794db6ad3079909996e31a1760e1642c
dev-dong-su <fkdlrj1230@gmail.com>
2022-12-08 21:20:40 +0900

    :white_check_mark: Update Join.js, Login.js, useInput.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import AccountPage from "./pages/AccountPage";
+import TodoPage from './pages/ToDoPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <AccountPage />,
   },
+  {
+    path: '/todo',
+    element: <TodoPage />
+  }
 ]);
 function App() {
   return (
